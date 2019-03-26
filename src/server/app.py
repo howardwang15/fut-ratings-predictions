1ac83e3d5c9ede272bd30b0fa029e45f0b246f4c
howardwang15 <joy_luck_2000@hotmail.com>
2019-03-26 14:53:15 -0700

    finished getting predictions by changing dimensionality of input vectors and returned the result to client

--- src/server/app.py
+++ src/server/app.py
@@ -1,8 +1,8 @@
 import os
 from flask import Flask, jsonify
 from dotenv import load_dotenv
-import json
 import requests
+import json
 import numpy as np
 
 load_dotenv()
@@ -14,27 +14,18 @@ def test():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # TODO: update the model to take in numpy arrays
-    stats = np.array([90, 92, 84, 91, 40, 76, 3])
-    stats = stats.reshape(1, 7)
-    list_stats = stats.tolist()
-    print(list_stats)
-    print(np.array(list_stats))
+    stats = [90, 92, 84, 91, 40, 76, 3]
     payload = {
         "instances": [
             {
-                "stats": list_stats
+                "stats": stats
             }
         ]
     }
     
     api_url = os.getenv("API_URL")
     model_path = api_url + "/v1/models/fut:predict"
-    print(model_path)
-    r = requests.post(model_path, json=payload)
-    print(r.status_code)
-    print(r.json())
-    print(r.content)
-    # response = json.loads(r.content)
-    # print(response)
-    return api_url 
+    response = requests.post(model_path, json=payload)
+    res = response.json()
+    prediction = res['predictions']
+    return json.dumps(prediction)
