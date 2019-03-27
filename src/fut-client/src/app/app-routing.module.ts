ddc88294df163b6282e41392122840ce57581659
howardwang15 <joy_luck_2000@hotmail.com>
2019-03-27 13:57:34 -0700

    finished topbar and added bootstrap

--- src/fut-client/src/app/app-routing.module.ts
+++ src/fut-client/src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: '/about',
+    redirectTo: '/app',
     pathMatch: 'full'
   }
 ];
--- src/fut-client/src/app/app.module.ts
+++ src/fut-client/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +18,9 @@ import { CardComponent } from './card/card.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
