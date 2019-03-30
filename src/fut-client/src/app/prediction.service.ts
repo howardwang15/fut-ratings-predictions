7f1890443a374e73cbba236444c67ac27c3ce08b
howardwang15 <joy_luck_2000@hotmail.com>
2019-03-30 00:09:29 -0700

    added card types radio buttons and conditional rendering of cards

--- src/fut-client/src/app/prediction.service.ts
+++ src/fut-client/src/app/prediction.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Response } from './response';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable({
@@ -9,10 +9,10 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 export class PredictionService {
 
   private errorSource = new BehaviorSubject<string>("");
-  private prediction = new BehaviorSubject<Number>(0);
+  private predictionSource = new BehaviorSubject<Number>(0);
 
   currentError = this.errorSource.asObservable();
-  currentPrediction = this.prediction.asObservable();
+  currentPrediction = this.predictionSource.asObservable();
 
   private apiUrl = "http://localhost:5000/predict";
   private httpOptions = {
@@ -21,15 +21,20 @@ export class PredictionService {
 
   constructor(private http: HttpClient) { }
 
-  predict(stats: Object): Observable<Object> {
+  predict(stats: Object) {
     let response = this.http.post<Object>(this.apiUrl, stats, this.httpOptions);
-    response.subscribe(res => {
+    response.subscribe((res: Response) => {
       this.updateErrorMessage(res.error);
+      if (!res.error)
+        this.updatePrediction(res.prediction);
     });
-    return response;
   }
 
   updateErrorMessage(error: string) {
     this.errorSource.next(error);
+  }
+
+  updatePrediction(prediction: Number) {
+    this.predictionSource.next(prediction);
   }
 }
